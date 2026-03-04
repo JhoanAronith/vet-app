547410d934d7b065282ff452943904f50a390d5e
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-03 22:11:37 -0500

    Creation of a repository for the Service entity

---
+++ src/main/java/com/aronith/vet/repository/ServicioReposiroty.java
@@ -0,0 +1,8 @@
+package com.aronith.vet.repository;
+
+import com.aronith.vet.model.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicioReposiroty extends JpaRepository<Servicio, Long> {
+
+}
