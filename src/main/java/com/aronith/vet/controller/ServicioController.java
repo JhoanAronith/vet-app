3a0c6ed26df3e13ae2cab7078c76d348be6594a6
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-03 23:49:39 -0500

    creation of the controller for the Service entity

---
+++ src/main/java/com/aronith/vet/controller/ServicioController.java
@@ -0,0 +1,32 @@
+package com.aronith.vet.controller;
+
+import com.aronith.vet.dto.request.ServicioRequestDTO;
+import com.aronith.vet.dto.response.ServicioResponseDTO;
+import com.aronith.vet.service.ServicioService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/servicio")
+@AllArgsConstructor
+public class ServicioController {
+
+    private final ServicioService servicioService;
+
+    @PostMapping("/guardar")
+    public ResponseEntity<ServicioResponseDTO> guardar(@RequestBody ServicioRequestDTO dto) {
+        ServicioResponseDTO response = servicioService.guardar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<ServicioResponseDTO>> listar() {
+        return ResponseEntity.ok(servicioService.listarTodos());
+    }
+
+
+}
