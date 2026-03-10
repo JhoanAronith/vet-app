74c0453c1fd73e31cc95993729ac5b828a2803c7
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-10 07:22:11 -0500

    Creación de ErrorResponseDTO para el manejo personalizado de excepciones

---
+++ src/main/java/com/aronith/vet/dto/response/ErrorResponseDTO.java
@@ -0,0 +1,11 @@
+package com.aronith.vet.dto.response;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponseDTO(
+    LocalDateTime timestamp,
+    int status,
+    String error,
+    String message,
+    String path
+) { }
