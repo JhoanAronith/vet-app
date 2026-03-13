a8b6df1071b233aa3f53bd450918c51bbc0ed422
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-12 22:06:41 -0500

    Validaciones para ServicioDTO

--- src/main/java/com/aronith/vet/dto/request/ServicioRequestDTO.java
+++ src/main/java/com/aronith/vet/dto/request/ServicioRequestDTO.java
@@ -1,17 +1,22 @@
 package com.aronith.vet.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Información para el registro de servicios veterinarios (Consultas, Cirugías, Baños, etc.)")
 public record ServicioRequestDTO(
 
         @Schema(description = "Nombre del servicio prestado", example = "Consulta General")
+        @NotBlank(message = "El nombre es obligatorio")
         String nombre,
 
         @Schema(description = "Descripción detallada de lo que incluye el servicio",
                 example = "Evaluación física completa, toma de temperatura y pesaje.")
+        @NotBlank(message = "La descripcion es obligatoria")
         String descripcion,
 
         @Schema(description = "Costo base del servicio", example = "45.00")
+        @NotNull(message = "El precio no puede ser null")
         Double precio
 ) {}
