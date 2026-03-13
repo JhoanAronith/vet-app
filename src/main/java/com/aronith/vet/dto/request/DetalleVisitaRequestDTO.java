d88aec134557e07347657b672f6a441c6c032a62
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-12 21:57:22 -0500

    Validaciones para DetalleVisitaDTO

--- src/main/java/com/aronith/vet/dto/request/DetalleVisitaRequestDTO.java
+++ src/main/java/com/aronith/vet/dto/request/DetalleVisitaRequestDTO.java
@@ -1,19 +1,24 @@
 package com.aronith.vet.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Detalle individual de los servicios o medicamentos aplicados durante una visita")
 public record DetalleVisitaRequestDTO(
 
         @Schema(description = "Cantidad del ítem aplicado (servicio o medicamento)", example = "1")
+        @NotNull(message = "La cantidad no puede ser null")
         Integer cantidad,
 
         @Schema(description = "Precio final cobrado (puede diferir del precio base)", example = "25.50")
+        @NotNull(message = "La precio aplicado no puede ser null")
         Double precioAplicado,
 
         @Schema(description = "ID del servicio realizado (opcional si es medicamento)", example = "4")
+        @NotNull(message = "El ID del servicio no puede ser null")
         Long idServicio,
 
         @Schema(description = "ID del medicamento suministrado (opcional si es servicio)", example = "12")
+        @NotNull(message = "El ID del medicamento no puede ser null")
         Long idMedicamento
 ) { }
