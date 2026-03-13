2ee91007f4d9f8a5a32e1c34848d0b115b317eb4
Jhoan Muñoz <jhoanmunozman@gmail.com>
2026-03-12 21:47:48 -0500

    Solución de error en dependencias de validación y pruebas en DTO de Citas

--- src/main/java/com/aronith/vet/dto/request/CitaRequestDTO.java
+++ src/main/java/com/aronith/vet/dto/request/CitaRequestDTO.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDateTime;
 
 @Schema(description = "DTO para la solicitud de creación de una nueva cita médica")
@@ -13,7 +15,8 @@ public record CitaRequestDTO (
                 description = "Fecha y hora programada para la cita (Formato ISO)",
                 example = "2026-05-20T10:30:00"
         )
-        @NotNull @Future
+        @NotNull(message = "La fecha y hora son obligatorios")
+        @Future(message = "La cita debe programarse para una fecha futura")
         LocalDateTime fechaHora,
 
         @Schema(
@@ -22,13 +25,14 @@ public record CitaRequestDTO (
                 minLength = 5,
                 maxLength = 255
         )
-        @NotBlank
+        @NotBlank(message = "El motivo no puede estar vacío")
+        @Size(min = 5, max = 255, message = "El motivo debe tener entre 5 y 255 caracteres.")
         String motivo,
 
         @Schema(
                 description = "ID único de la mascota que recibirá la atención",
                 example = "1"
         )
-        @NotNull
+        @NotNull(message = "El ID de la mascota el obligatorio")
         Long idMascota
 ){ }
